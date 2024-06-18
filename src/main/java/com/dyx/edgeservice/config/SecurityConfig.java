94de2681034feac1f07bed93585cb8d3fe1c486a
duanyunxiang <duanyunxiang@tom.com>
2024-06-18 13:34:37 +0800

    登出handler

--- src/main/java/com/dyx/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/dyx/edgeservice/config/SecurityConfig.java
@@ -4,13 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 //启用spring security webFlux支持
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http){
+    //ReactiveClientRegistrationRepository是注册到Keycloak中的客户端
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository){
         return http.authorizeExchange(exchange->
                         //所有请求都进行验证
                         exchange.anyExchange().authenticated())
@@ -19,6 +23,15 @@ public class SecurityConfig {
 
                 //认证模式二：使用OAuth2/OpenID Connect启用用户认证，由Keycloak提供登录页面
                 .oauth2Login(Customizer.withDefaults())
+                // 定义退出成功时的handler
+                .logout(logout-> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)))
                 .build();
+    }
+
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(ReactiveClientRegistrationRepository clientRegistrationRepository){
+        var oidcLogoutSuccessHandler=new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+        //重定向至应用baseUrl，由Spring动态计算获得，本地是http://localhost:9000/
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+        return oidcLogoutSuccessHandler;
     }
 }
