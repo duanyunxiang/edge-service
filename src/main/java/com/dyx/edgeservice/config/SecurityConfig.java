58d3baad63fdacd49fe669959ddc3821e5dc75a3
duanyunxiang <duanyunxiang@tom.com>
2024-06-18 10:41:11 +0800

    使用spring security

---
+++ src/main/java/com/dyx/edgeservice/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.dyx.edgeservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+//启用spring security webFlux支持
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http){
+        return http.authorizeExchange(exchange->
+                        //所有请求都进行验证
+                        exchange.anyExchange().authenticated())
+                //通过登录表单启用用户认证，框架内置登录页面，内置用户user（密码见启动日志）
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
