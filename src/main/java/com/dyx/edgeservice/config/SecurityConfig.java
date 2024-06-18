712e5685d5e75aec9a960251345cc95babad6555
duanyunxiang <duanyunxiang@tom.com>
2024-06-18 12:58:54 +0800

    使用OAuth2客户端

--- src/main/java/com/dyx/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/dyx/edgeservice/config/SecurityConfig.java
@@ -14,8 +14,11 @@ public class SecurityConfig {
         return http.authorizeExchange(exchange->
                         //所有请求都进行验证
                         exchange.anyExchange().authenticated())
-                //通过登录表单启用用户认证，框架内置登录页面，内置用户user（密码见启动日志）
-                .formLogin(Customizer.withDefaults())
+                //认证模式一：通过登录表单启用用户认证，框架内置登录页面，内置用户user（密码见启动日志）
+                //.formLogin(Customizer.withDefaults())
+
+                //认证模式二：使用OAuth2/OpenID Connect启用用户认证，由Keycloak提供登录页面
+                .oauth2Login(Customizer.withDefaults())
                 .build();
     }
 }
